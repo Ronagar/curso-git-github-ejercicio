2cd044bd9ed643fe88ef0dfa42bc3383d3ab5e2e
Roberto <rnginformatica@uma.es>
2021-03-07 17:52:56 +0100

    Salida final modificada
    Se ha modificado la salida final en la que aparecian los participantes
    del curso

--- Proyecto/src/Main.java
+++ Proyecto/src/Main.java
@@ -3,14 +3,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Inicio del programa.\n");
 
-        System.out.println("Miembros del curso:");
+        System.out.println("Miembros del curso \t de Git & GitHub:");
         
-        System.out.println("Diego.");
-        System.out.println("Narta.");
-        System.out.println("pedro");
-        System.out.println("Ivan.");
-        System.out.println("Manu");
+        System.out.println("1 - Diego.");
+        System.out.println("2 - Edu.");
+        System.out.println("3 - Iván.");
+        System.out.println("4 - Javi.");
+        System.out.println("5 - Manuel.");
+        System.out.println("6 - Marta.");
+        System.out.println("7 - Pedro.");
+        System.out.println("8 - Roberto.");
 
         System.out.println("\nFinal del programa.");
     }
 }
+
+
+
+    
+    
+    
+    
+    
+    
